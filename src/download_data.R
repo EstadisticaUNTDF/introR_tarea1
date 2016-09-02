a8fb4853e7c3d4083397a85fcfca85d453d36687
Luciano Selzer <luciano.selzer@gmail.com>
2016-09-02 10:18:37 -0300

    Quitados caracteres BOM de los nombres

--- src/download_data.R
+++ src/download_data.R
@@ -14,3 +14,7 @@ URL <- res$results[[1]]$url
 
 fil <- paste0("data/", basename(URL)) 
 if (!file.exists(fil)) download.file(URL, fil) 
+
+data <- read.csv(fil, encoding = "UTF-8")
+names(data) <- sub("X\\.U\\.FEFF\\.", "", names(data))
+write.csv(data, fil, row.names = FALSE)
